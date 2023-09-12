0089d936ca7680eeba6155db9432e832671272ef
Sudipta Karmakar <xsudiptaster@gmail.com>
2023-09-12 19:22:58 +0200

    Fix the Report Type Bug of Field selection

--- src/components/ReportTypeView/FieldDisplayPanelView/FieldDisplayPanelView.util.ts
+++ src/components/ReportTypeView/FieldDisplayPanelView/FieldDisplayPanelView.util.ts
@@ -1,30 +1,30 @@
 const getAllColumns = (recodTypeDescribe: any) => {
-   let columns: any[] = [];
-   recodTypeDescribe.sections.forEach((section: any) => {
-      columns = [...columns, ...section.columns];
-   });
-   return columns;
+  let columns: any[] = [];
+  recodTypeDescribe.sections.forEach((section: any) => {
+    columns = [...columns, ...section.columns];
+  });
+  return columns;
 };
 const checkField = (field: any, fieldPrefix: string, tableName: string, columns: any[]) => {
-   let selected = columns.some((column) => {
-      let fullField =
-         fieldPrefix === "" ? field.name : fieldPrefix + "." + field.type === "reference" ? field.relationshipName : field.name;
-      if (column.table === tableName && column.field === fullField) {
-         return true;
-      } else {
-         return false;
-      }
-   });
-   return selected;
+  let selected = columns.some((column) => {
+    let fullField =
+      fieldPrefix !== "" ? fieldPrefix + "." + (field.type === "reference" ? field.relationshipName : field.name) : field.name;
+    if (column.table === tableName && column.field === fullField) {
+      return true;
+    } else {
+      return false;
+    }
+  });
+  return selected;
 };
 export const seggrateFields = (recodTypeDescribe: any, fields: any, tableName: string, fieldPrefix: any) => {
-   let columns = getAllColumns(recodTypeDescribe);
-   let displayFields: any = [];
-   fields.forEach((field: any) => {
-      let selected = checkField(field, fieldPrefix, tableName, columns);
-      displayFields.push({ ...field, selected });
-   });
-   return displayFields.sort((a: any, b: any) => {
-      return a.label > b.label ? 1 : -1;
-   });
+  let columns = getAllColumns(recodTypeDescribe);
+  let displayFields: any = [];
+  fields.forEach((field: any) => {
+    let selected = checkField(field, fieldPrefix, tableName, columns);
+    displayFields.push({ ...field, selected });
+  });
+  return displayFields.sort((a: any, b: any) => {
+    return a.label > b.label ? 1 : -1;
+  });
 };
--- src/components/ReportTypeView/ReportTypeFieldView/ReportTypeFieldView.tsx
+++ src/components/ReportTypeView/ReportTypeFieldView/ReportTypeFieldView.tsx
@@ -1,5 +1,6 @@
 import { Badge, Col, Input, Modal, Row, Tabs } from "antd";
 import React from "react";
+import { BiSearch } from "react-icons/bi";
 import { useRecoilState } from "recoil";
 import { reportFieldTreeDataAtom, selectedReportTypeAtom } from "../../../atoms/atom";
 import FieldDisplayPanelView from "../FieldDisplayPanelView";
@@ -8,99 +9,100 @@ import { addNewSection, countField, removeSection } from "./ReportTypeFieldView.
 import TabHeaderView from "./TabHeaderView";
 
 interface IReportTypeFieldViewProps {
-   children?: React.ReactNode;
+  children?: React.ReactNode;
 }
 
 const ReportTypeFieldView: React.FC<IReportTypeFieldViewProps> = (props) => {
-   const [selectedReportType, setSelectedReportType] = useRecoilState(selectedReportTypeAtom);
-   const [objectFields] = useRecoilState(reportFieldTreeDataAtom);
-   const [searchStringField, setSearchStringField] = React.useState("");
-   const [searchString, setSearchString] = React.useState("");
-   const { confirm } = Modal;
-   const onEditClick = async (tabId: any, method: any) => {
-      if (method === "remove") {
-         confirm({
-            content: "Are you sure you want to delete the section?",
-            onOk() {
-               let response = removeSection(selectedReportType, tabId);
-               setSelectedReportType(response);
-            },
-            onCancel() {
-               console.log("Cancel");
-            },
-         });
-      } else {
-         let response = addNewSection(selectedReportType);
-         setSelectedReportType(response);
-      }
-   };
-   return (
-      <>
-         <Row>
-            <Col span={17}>
-               <Tabs
-                  type="editable-card"
-                  tabBarGutter={1}
-                  tabPosition="right"
-                  destroyInactiveTabPane={true}
+  const [selectedReportType, setSelectedReportType] = useRecoilState(selectedReportTypeAtom);
+  const [objectFields] = useRecoilState(reportFieldTreeDataAtom);
+  const [searchStringField, setSearchStringField] = React.useState("");
+  const [searchString, setSearchString] = React.useState("");
+  const { confirm } = Modal;
+  const onEditClick = async (tabId: any, method: any) => {
+    if (method === "remove") {
+      confirm({
+        content: "Are you sure you want to delete the section?",
+        onOk() {
+          let response = removeSection(selectedReportType, tabId);
+          setSelectedReportType(response);
+        },
+        onCancel() {
+          console.log("Cancel");
+        },
+      });
+    } else {
+      let response = addNewSection(selectedReportType);
+      setSelectedReportType(response);
+    }
+  };
+  return (
+    <>
+      <Row>
+        <Col span={17}>
+          <Input
+            placeholder="Seach Field"
+            size="small"
+            onChange={(e) => {
+              setSearchString(e.target.value);
+            }}
+            addonAfter={<BiSearch />}
+          />
+          <Tabs
+            style={{ paddingTop: "10px" }}
+            type="editable-card"
+            tabBarGutter={1}
+            tabPosition="right"
+            destroyInactiveTabPane={true}
+            size="small"
+            animated
+            onEdit={(e, p) => {
+              onEditClick(e, p);
+            }}
+            items={
+              selectedReportType?.sections
+                ? selectedReportType?.sections.map((section: any, index: number) => {
+                    return {
+                      label: (
+                        <Badge count={countField(section, searchString)}>
+                          <TabHeaderView section={section} index={index} />
+                        </Badge>
+                      ),
+                      key: index,
+                      children: <ReportFieldDisplayView section={section} searchString={searchString} index={index} />,
+                    };
+                  })
+                : []
+            }
+          />
+        </Col>
+        <Col span={7}>
+          <Tabs
+            size="small"
+            style={{ paddingLeft: "20px" }}
+            destroyInactiveTabPane={true}
+            items={objectFields.map((reportObject) => {
+              return {
+                label: reportObject.tableName,
+                key: reportObject.tableName,
+                children: <FieldDisplayPanelView reportObject={reportObject} searchStringField={searchStringField} />,
+              };
+            })}
+            tabBarExtraContent={
+              <>
+                <Input
                   size="small"
-                  animated
-                  onEdit={(e, p) => {
-                     onEditClick(e, p);
+                  placeholder="Search Field"
+                  onChange={(e) => {
+                    setSearchStringField(e.target.value);
                   }}
-                  tabBarExtraContent={
-                     <Input
-                        size="small"
-                        placeholder="Search Field"
-                        onChange={(e) => {
-                           setSearchString(e.target.value);
-                        }}
-                     />
-                  }
-                  items={
-                     selectedReportType?.sections
-                        ? selectedReportType?.sections.map((section: any, index: number) => {
-                             return {
-                                label: (
-                                   <Badge count={countField(section, searchString)}>
-                                      <TabHeaderView section={section} index={index} />
-                                   </Badge>
-                                ),
-                                key: index,
-                                children: <ReportFieldDisplayView section={section} searchString={searchString} index={index} />,
-                             };
-                          })
-                        : []
-                  }
-               />
-            </Col>
-            <Col span={7}>
-               <Tabs
-                  size="small"
-                  style={{ paddingLeft: "20px" }}
-                  items={objectFields.map((reportObject) => {
-                     return {
-                        label: reportObject.tableName,
-                        key: reportObject.tableName,
-                        children: <FieldDisplayPanelView reportObject={reportObject} searchStringField={searchStringField} />,
-                     };
-                  })}
-                  tabBarExtraContent={
-                     <>
-                        <Input
-                           size="small"
-                           placeholder="Search Field"
-                           onChange={(e) => {
-                              setSearchStringField(e.target.value);
-                           }}
-                        />
-                     </>
-                  }
-               />
-            </Col>
-         </Row>
-      </>
-   );
+                />
+              </>
+            }
+          />
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ReportTypeFieldView;
