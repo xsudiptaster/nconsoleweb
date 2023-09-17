991561701e79e5082fe9accc27ce8a508b78a329
Sudipta Karmakar <xsudiptaster@gmail.com>
2023-09-17 23:44:36 +0200

    update background image

--- src/components/HomePageView/HomePageView.tsx
+++ src/components/HomePageView/HomePageView.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Row, Typography, Watermark } from "antd";
+import { Card, Col, Row, Typography } from "antd";
 import React from "react";
 import ApexCodeDescriptionView from "./ApexCodeDescriptionView";
 import CountryPicklistDescriptionView from "./CountryPicklistDescriptionView";
@@ -18,129 +18,127 @@ const { Title, Paragraph, Link, Text } = Typography;
 const HomePageView: React.FC<IHomePageViewProps> = (props) => {
    return (
       <Card size="small" bodyStyle={{ maxHeight: "85vh", overflow: "auto", backgroundImage: "../../nconsoleimg_edge.png" }}>
-         <Watermark image={"../../nconsoleimg_edge.png"}>
-            <div className={style.contentDiv}>
-               <Title>NConsole</Title>
-               <Text strong>Making Salesforce Administration Easier</Text>
-               <br />
-               <img width={500} src="../../nconsoleimg.png" alt="logo.png" />
-            </div>
-            <div className={style.contentDiv}>
-               <Title>Features</Title>
-            </div>
-            <Row gutter={[16, 16]}>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <DiagramDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <ReportTypeDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <ReportFolderDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <DashboardFolderDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <ApexCodeDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <QueryDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <CountryPicklistDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <CustomMetadataDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <PermissionsCompareDescriptionView />
-               </Col>
-               <Col
-                  span={12}
-                  style={{
-                     alignContent: "center",
-                     borderRadius: "30px",
-                     background: " #212121",
-                     boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
-                  }}
-               >
-                  <PermissionEditDescriptionView />
-               </Col>
-            </Row>
-         </Watermark>
+         <div className={style.contentDiv}>
+            <Title>NConsole</Title>
+            <Text strong>Making Salesforce Administration Easier</Text>
+            <br />
+            <img width={500} src="../../nconsoleimg.png" alt="logo.png" />
+         </div>
+         <div className={style.contentDiv}>
+            <Title>Features</Title>
+         </div>
+         <Row gutter={[16, 16]}>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <DiagramDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <ReportTypeDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <ReportFolderDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <DashboardFolderDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <ApexCodeDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <QueryDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <CountryPicklistDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <CustomMetadataDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <PermissionsCompareDescriptionView />
+            </Col>
+            <Col
+               span={12}
+               style={{
+                  alignContent: "center",
+                  borderRadius: "30px",
+                  background: " #212121",
+                  boxShadow: "15px 15px 30px rgb(25, 25, 25),-15px -15px 30px rgb(60, 60, 60)",
+               }}
+            >
+               <PermissionEditDescriptionView />
+            </Col>
+         </Row>
       </Card>
    );
 };
--- src/components/LoginModalView/LoginModalView.tsx
+++ src/components/LoginModalView/LoginModalView.tsx
@@ -1,52 +1,55 @@
 import { Button, Modal, Space } from "antd";
 import React from "react";
+import "../../nconsoleimg_edge.png";
 import { handleLoad, handleProdLogin, handleTestLogin } from "./LoginModalView.util";
 import "./style.css";
 interface ILoginModalViewProps {
-  children?: React.ReactNode;
+   children?: React.ReactNode;
 }
 
 const LoginModalView: React.FC<ILoginModalViewProps> = (props) => {
-  const [state, setState] = React.useState<any>("");
-  React.useEffect(() => {
-    const onload = () => {
-      let response = handleLoad();
-      setState(response);
-    };
-    onload();
-  }, []);
-  return (
-    <>
-      <Modal
-        className="modalGlass"
-        open={true}
-        footer={null}
-        closable={false}
-        title={<div style={{ textAlign: "center" }}>Login</div>}
-      >
-        <Space direction="vertical" style={{ width: "100%" }} size="large">
-          <Button
-            block
-            size="large"
-            onClick={() => {
-              handleProdLogin(state);
-            }}
-          >
-            Production
-          </Button>
-          <Button
-            block
-            size="large"
-            onClick={() => {
-              handleTestLogin(state);
-            }}
-          >
-            Sandbox
-          </Button>
-        </Space>
-      </Modal>
-    </>
-  );
+   const [state, setState] = React.useState<any>("");
+   React.useEffect(() => {
+      const onload = () => {
+         let response = handleLoad();
+         setState(response);
+      };
+      onload();
+   }, []);
+   return (
+      <>
+         <div style={{ backgroundImage: "../../nconsoleimg_edge.png" }}>
+            <Modal
+               className="modalGlass"
+               open={true}
+               footer={null}
+               closable={false}
+               title={<div style={{ textAlign: "center" }}>Login</div>}
+            >
+               <Space direction="vertical" style={{ width: "100%" }} size="large">
+                  <Button
+                     block
+                     size="large"
+                     onClick={() => {
+                        handleProdLogin(state);
+                     }}
+                  >
+                     Production
+                  </Button>
+                  <Button
+                     block
+                     size="large"
+                     onClick={() => {
+                        handleTestLogin(state);
+                     }}
+                  >
+                     Sandbox
+                  </Button>
+               </Space>
+            </Modal>
+         </div>
+      </>
+   );
 };
 
 export default LoginModalView;
