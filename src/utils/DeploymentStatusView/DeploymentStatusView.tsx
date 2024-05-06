8e6c837dd34adbf1015cc9bda9a6ae29a8caee27
Sudipta Karmakar <xsudiptaster@gmail.com>
2024-05-06 15:55:08 +0200

    updated Deployment Status

--- src/utils/DeploymentStatusView/DeploymentStatusView.tsx
+++ src/utils/DeploymentStatusView/DeploymentStatusView.tsx
@@ -2,6 +2,7 @@ import { Col, Modal, Row, Statistic, Tabs } from "antd";
 import React from "react";
 import DisplayExcelSheetView from "../DisplayExcelSheetView";
 import RenderIf from "../RenderIf";
+import { cleanDeploymentStatus } from "./DeploymentStatusView.util";
 
 interface IDeploymentStatusViewProps {
    children?: React.ReactNode;
@@ -12,11 +13,13 @@ interface IDeploymentStatusViewProps {
 
 const DeploymentStatusView: React.FC<IDeploymentStatusViewProps> = (props) => {
    const { displayResult, open, setOpen } = props;
-
+   const cleanedResult = React.useMemo(() => {
+      return cleanDeploymentStatus(displayResult);
+   }, [displayResult]);
    return (
       <>
          <Modal
-            title="Basic Modal"
+            title="Deployment Status"
             style={{ width: 1000 }}
             width={1000}
             open={open}
@@ -31,62 +34,31 @@ const DeploymentStatusView: React.FC<IDeploymentStatusViewProps> = (props) => {
                   {
                      key: "componentFailures",
                      label: "Component Failures",
-                     children: (
-                        <DisplayExcelSheetView
-                           data={displayResult?.details?.componentFailures ? displayResult?.details?.componentFailures : []}
-                           showDownload
-                        />
-                     ),
+                     children: <DisplayExcelSheetView data={cleanedResult.failures} showDownload />,
                   },
                   {
                      key: "componentSuccesses",
                      label: "Component Successes",
-                     children: (
-                        <DisplayExcelSheetView
-                           data={displayResult?.details?.componentSuccesses ? displayResult?.details?.componentSuccesses : []}
-                           showDownload
-                        />
-                     ),
+                     children: <DisplayExcelSheetView data={cleanedResult.successes} showDownload />,
                   },
                   {
                      key: "runTestResult",
                      label: "Test Failure",
                      children: (
                         <>
-                           <RenderIf renderIf={displayResult?.details?.runTestResult !== undefined}>
+                           <RenderIf renderIf={cleanedResult.runTest}>
                               <Row gutter={16}>
                                  <Col span={12}>
-                                    <Statistic
-                                       title="Failures"
-                                       value={
-                                          displayResult?.details?.runTestResult.numFailures
-                                             ? parseInt(displayResult?.details?.runTestResult.numFailures)
-                                             : 0
-                                       }
-                                    />
+                                    <Statistic title="Failures" value={cleanedResult.runTest.numFailures} />
                                  </Col>
                                  <Col span={12}>
-                                    <Statistic
-                                       title="Test Runs"
-                                       value={
-                                          displayResult?.details?.runTestResult?.numTestsRun
-                                             ? parseInt(displayResult?.details?.runTestResult?.numTestsRun)
-                                             : 0
-                                       }
-                                    />
+                                    <Statistic title="Test Runs" value={cleanedResult.runTest.numTestsRun} />
                                  </Col>
                                  <Col span={12}>
-                                    <Statistic
-                                       title="Total Time"
-                                       value={
-                                          displayResult?.details?.runTestResult?.totalTime
-                                             ? parseInt(displayResult?.details?.runTestResult?.totalTime)
-                                             : 0
-                                       }
-                                    />
+                                    <Statistic title="Total Time" value={cleanedResult.runTest.totalTime} />
                                  </Col>
                                  <Col span={24}>
-                                    <RenderIf renderIf={displayResult?.details?.runTestResult?.codeCoverage !== undefined}>
+                                    <RenderIf renderIf={cleanedResult.runTest.codeCoverage !== undefined}>
                                        <Tabs
                                           items={[
                                              {
@@ -94,17 +66,7 @@ const DeploymentStatusView: React.FC<IDeploymentStatusViewProps> = (props) => {
                                                 label: "Code Coverage",
                                                 children: (
                                                    <DisplayExcelSheetView
-                                                      data={
-                                                         displayResult?.details?.runTestResult?.codeCoverage
-                                                            ? displayResult?.details?.runTestResult?.codeCoverage.map(
-                                                                 (cov: any) => {
-                                                                    delete cov.locationsNotCovered;
-                                                                    delete cov.namespace;
-                                                                    return cov;
-                                                                 }
-                                                              )
-                                                            : []
-                                                      }
+                                                      data={cleanedResult.runTest.codeCoverage}
                                                       showDownload
                                                    />
                                                 ),
@@ -114,16 +76,7 @@ const DeploymentStatusView: React.FC<IDeploymentStatusViewProps> = (props) => {
                                                 label: "Flow Coverage",
                                                 children: (
                                                    <DisplayExcelSheetView
-                                                      data={
-                                                         displayResult?.details?.runTestResult?.flowCoverage
-                                                            ? displayResult?.details?.runTestResult?.flowCoverage.map(
-                                                                 (cov: any) => {
-                                                                    delete cov.flowNamespace;
-                                                                    return cov;
-                                                                 }
-                                                              )
-                                                            : []
-                                                      }
+                                                      data={cleanedResult.runTest.flowCoverage}
                                                       showDownload
                                                    />
                                                 ),
@@ -132,17 +85,7 @@ const DeploymentStatusView: React.FC<IDeploymentStatusViewProps> = (props) => {
                                                 key: "failures",
                                                 label: "Failures",
                                                 children: (
-                                                   <DisplayExcelSheetView
-                                                      data={
-                                                         displayResult?.details?.runTestResult?.failures
-                                                            ? displayResult?.details?.runTestResult?.failures.map((cov: any) => {
-                                                                 delete cov.namespace;
-                                                                 return cov;
-                                                              })
-                                                            : []
-                                                      }
-                                                      showDownload
-                                                   />
+                                                   <DisplayExcelSheetView data={cleanedResult.runTest.failures} showDownload />
                                                 ),
                                              },
 
@@ -150,17 +93,7 @@ const DeploymentStatusView: React.FC<IDeploymentStatusViewProps> = (props) => {
                                                 key: "successes",
                                                 label: "Successes",
                                                 children: (
-                                                   <DisplayExcelSheetView
-                                                      data={
-                                                         displayResult?.details?.runTestResult?.successes
-                                                            ? displayResult?.details?.runTestResult?.successes.map((cov: any) => {
-                                                                 delete cov.namespace;
-                                                                 return cov;
-                                                              })
-                                                            : []
-                                                      }
-                                                      showDownload
-                                                   />
+                                                   <DisplayExcelSheetView data={cleanedResult.runTest.successes} showDownload />
                                                 ),
                                              },
                                           ]}
--- src/utils/DeploymentStatusView/DeploymentStatusView.util.ts
+++ src/utils/DeploymentStatusView/DeploymentStatusView.util.ts
@@ -1,1 +1,89 @@
-export const checkDeploymentStatuses = async () => {};
+export const cleanDeploymentStatus = (deploymentResponse: any) => {
+   let obj: any = {
+      failures: [],
+      successes: [],
+      runTest: {
+         numFailures: 0,
+         numTestsRun: 0,
+         totalTime: 0,
+         codeCoverage: [],
+         flowCoverage: [],
+         failures: [],
+         successes: [],
+      },
+   };
+   if (deploymentResponse?.details?.componentFailures && Array.isArray(deploymentResponse?.details?.componentFailures)) {
+      obj.failures = deploymentResponse?.details?.componentFailures;
+   } else if (deploymentResponse?.details?.componentFailures) {
+      obj.failures.push(deploymentResponse?.details?.componentFailures);
+   }
+   if (deploymentResponse?.details?.componentSuccesses && Array.isArray(deploymentResponse?.details?.componentSuccesses)) {
+      obj.successes = deploymentResponse?.details?.componentSuccesses;
+   } else if (deploymentResponse?.details?.componentSuccesses) {
+      obj.successes.push(deploymentResponse?.details?.componentSuccesses);
+   }
+   if (deploymentResponse?.details?.runTestResult.numFailures) {
+      obj.runTest.numFailures = parseInt(deploymentResponse?.details?.runTestResult.numFailures);
+   }
+   if (deploymentResponse?.details?.runTestResult?.numTestsRun) {
+      obj.runTest.numTestsRun = parseInt(deploymentResponse?.details?.runTestResult.numTestsRun);
+   }
+   if (deploymentResponse?.details?.runTestResult?.totalTime) {
+      obj.runTest.totalTime = parseInt(deploymentResponse?.details?.runTestResult.totalTime);
+   }
+   if (
+      deploymentResponse?.details?.runTestResult?.codeCoverage &&
+      Array.isArray(deploymentResponse?.details?.runTestResult?.codeCoverage)
+   ) {
+      obj.runTest.codeCoverage = deploymentResponse?.details?.runTestResult?.codeCoverage.map((cov: any) => {
+         delete cov.locationsNotCovered;
+         delete cov.namespace;
+         return cov;
+      });
+   } else if (deploymentResponse?.details?.runTestResult?.codeCoverage) {
+      let tempObj = { ...deploymentResponse?.details?.runTestResult?.codeCoverage };
+      delete tempObj.locationsNotCovered;
+      delete tempObj.namespace;
+      obj.runTest.codeCoverage.push(tempObj);
+   }
+   if (
+      deploymentResponse?.details?.runTestResult?.flowCoverage &&
+      Array.isArray(deploymentResponse?.details?.runTestResult?.flowCoverage)
+   ) {
+      obj.runTest.flowCoverage = deploymentResponse?.details?.runTestResult?.flowCoverage.map((cov: any) => {
+         delete cov.flowNamespace;
+         return cov;
+      });
+   } else if (deploymentResponse?.details?.runTestResult?.flowCoverage) {
+      let tempObj = { ...deploymentResponse?.details?.runTestResult?.flowCoverage };
+      delete tempObj.flowNamespace;
+      obj.runTest.flowCoverage.push(tempObj);
+   }
+   if (
+      deploymentResponse?.details?.runTestResult?.failures &&
+      Array.isArray(deploymentResponse?.details?.runTestResult?.failures)
+   ) {
+      obj.runTest.failures = deploymentResponse?.details?.runTestResult?.failures.map((cov: any) => {
+         delete cov.namespace;
+         return cov;
+      });
+   } else if (deploymentResponse?.details?.runTestResult?.failures) {
+      let tempObj = { ...deploymentResponse?.details?.runTestResult?.failures };
+      delete tempObj.namespace;
+      obj.runTest.failures.push(tempObj);
+   }
+   if (
+      deploymentResponse?.details?.runTestResult?.successes &&
+      Array.isArray(deploymentResponse?.details?.runTestResult?.successes)
+   ) {
+      obj.runTest.successes = deploymentResponse?.details?.runTestResult?.successes.map((cov: any) => {
+         delete cov.namespace;
+         return cov;
+      });
+   } else if (deploymentResponse?.details?.runTestResult?.successes) {
+      let tempObj = { ...deploymentResponse?.details?.runTestResult?.successes };
+      delete tempObj.namespace;
+      obj.runTest.successes.push(tempObj);
+   }
+   return obj;
+};
